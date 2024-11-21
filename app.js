cc2a9b6301656478e7a010b8af87335991a5d47c
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2024-11-21 11:20:58 +0900

    commit

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST'],
-  credentials: true // 쿠키 허용
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   methods: ['GET', 'POST'],
+//   credentials: true // 쿠키 허용
+// }));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // 특정 출처 허용
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // 허용할 메서드
-//   res.header('Access-Control-Allow-Headers', 'Content-Type'); // 허용할 헤더
-//   res.header('Access-Control-Allow-Credentials', 'true'); // 쿠키 허용
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // 특정 출처 허용
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // 허용할 메서드
+  res.header('Access-Control-Allow-Headers', 'Content-Type'); // 허용할 헤더
+  res.header('Access-Control-Allow-Credentials', 'true'); // 쿠키 허용
+  next();
+});
 
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
