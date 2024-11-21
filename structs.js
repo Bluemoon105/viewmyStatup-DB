59f5eab525b4ad2dacc95ad0385d4f71aa1d9781
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2024-11-21 15:27:41 +0900

    commit

--- structs.js
+++ structs.js
@@ -2,7 +2,7 @@ import * as s from "superstruct";
 
 export const CreateInvest = s.object({
     name: s.string(),
-    investAmount: s.number(),
+    investAmount: s.string(),
     comment : s.string(),
     password : s.string(),
     startupId: s.number(),
