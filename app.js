66678124a66bbc93713f2be3fc98929aa87f299b
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2024-11-22 09:09:56 +0900

    commit

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-import * as dotenv from 'dotenv';
-import express from 'express';
-import { PrismaClient, Prisma } from '@prisma/client';
-import { CreateInvest, PatchInvest } from './structs.js';
-import cors from 'cors';
-import { assert } from 'superstruct';
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import orderByStartup from "./orderByFunction.js";
+import asyncHandler from "./asyncHandlerFunction.js";
+import paginationHandler from "./paginationHandler.js";
+import { CreateInvest, PatchInvest } from "./structs.js";
+import { assert } from "superstruct";
 
 dotenv.config();
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
 // const corsOption = {
 //   origin: [
@@ -25,7 +30,6 @@ app.use(express.json());
 //   credentials: true // 인증 정보(쿠키 등) 허용
 // }));
 
-app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend's origin
@@ -42,110 +46,137 @@ app.use((req, res, next) => {
 //   res.sendStatus(204);
 // });
 
-// function asyncHandler(handler) {
-//   return async function (req, res) {
-//     try {
-//       await handler(req, res);
-//     } catch (e) {
-//       if (e.name === 'StructError' ||
-//         e instanceof Prisma.PrismaClientValidationError
-//       ) {
-//         res.status(400).send({ message: e.message });
-//       } else if (
-//         e instanceof Prisma.PrismaClientKnownRequestError &&
-//         e.code === 'P2025'
-//       ) {
-//         res.sendStatus(404);
-//       } else {
-//         res.status(500).send({ message: e.message });
-//       }
-//     }
-//   };
-// }
 
 // BigInt 값을 문자열로 변환하여 JSON 응답 생성
 function replacer(key, value) {
   return typeof value === 'bigint' ? value.toString() : value;
 }
+app.get(
+  "/api/startups",
+  asyncHandler(async (req, res) => {
+    const { offset = 0, limit = 10, order = "id" } = req.query;
+    // validation은 값이 들어오자마자 검사를 해야 큰 실수를 줄일 수 있음.
+    const offsetNum = parseInt(offset);
+    const limitNum = parseInt(limit);
 
-app.get('/startups/comparison', async (req, res) => {
-  const { offset = 0, limit = 10, order = 'id' } = req.query;
-  let orderBy;
-  switch (order) {
-    case 'id':
-      orderBy = { id: 'asc' };
-      break;
-  }
-  try {
+    const orderBy = orderByStartup(order);
     const startups = await prisma.startup.findMany({
       orderBy,
-      skip: parseInt(offset),
-      take: parseInt(limit),
-    }); // BigInt 값을 문자열로 변환하여 JSON 응답 생성 
-    const serializedStartups = JSON.stringify(startups, replacer); res.send(serializedStartups);
-    
-  } catch (error) { res.status(500).send({ message: error.message }); }
-});
+      skip: offsetNum,
+      take: limitNum,
+      include: { Category: true },
+    });
 
-//검색 기능
-app.get("/startups", async (req, res) => {
-  const {searchKeyword ,offset = 0, limit=10} =req.query;
-  try{
-    const startup = await prisma.startup.findMany({
+    const responseData = await paginationHandler(startups, offsetNum, limitNum);
+
+    // BigInt 값을 문자열로 변환하여 JSON 응답 생성
+    res.send(JSON.stringify(responseData, replacer));
+  })
+);
+
+app.get("/api/startups/search", async (req, res) => {
+  const { searchKeyword, offset = 0, limit = 10 } = req.query;
+  const offsetNum = parseInt(offset);
+  const limitNum = parseInt(limit);
+
+  const replacer = (key, value) =>
+    typeof value === "bigint" ? value.toString() : value;
+
+  try {
+    const totalCount = await prisma.startup.count({
       where: {
-        name: {contains:searchKeyword},
+        name: { contains: searchKeyword },
       },
-      skip: parseInt(offset),
-      take: parseInt(limit),
-    })
-    const serializedStartups = JSON.stringify(startup, replacer); res.send(serializedStartups);
-  }catch(error){ res. status(501).send({message: error.message});}
-})
+    });
+
+    const startups = await prisma.startup.findMany({
+      orderBy: { id: "asc" },
+      skip: offsetNum,
+      take: limitNum,
+      where: {
+        name: { contains: searchKeyword },
+      },
+    });
+
+    res.setHeader("X-Total-Count", totalCount);
+
+    const serializedStartups = JSON.stringify(startups, replacer);
+    res.send(serializedStartups);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
 
 //특정 기업 상세 조회
-app.get("/startups/:id", async (req, res) => {
-  const {id} = req.params;
-  const numId = parseInt(id, 10);
-  try {
-    const startup = await prisma.startup.findUnique({
-      where: { id: numId },
+app.get(
+  "/api/startups/:startupsId",
+  asyncHandler(async (req, res) => {
+    const { startupsId } = req.params;
+    const idNum = parseInt(startupsId, 10);
+    const startup = await prisma.startup.findUniqueOrThrow({
+      where: { id: idNum },
     });
-    const serializedStartups = JSON.stringify(startup, replacer); res.send(serializedStartups);
-  }catch(error) {res.status(404).send({message: error.message}); }
-});
+    res.send(JSON.stringify(startup, replacer));
+  })
+);
 
+app.get(
+  "/api/startups/:startupsId/rank",
+  asyncHandler(async (req, res) => {
+    const { startupsId } = req.params;
+    const idNum = parseInt(startupsId);
+    const startup = await prisma.startup.findMany({
+      where: { id: idNum },
+    });
+    res.send(JSON.stringify(startup, replacer));
+  })
+);
 
 //기업 선택 횟수 조회
-app.get('/selection', async (req, res) => {
-  const { offset = 0, limit = 10 } = req.query;
-  try{
-    const select = await prisma.startup.findMany({
-      skip: parseInt(offset),
-      take: parseInt(limit),
-      orderBy: {id: "asc"},
-      select: {
-        name:true, 
-        count:true,
-      },
+app.get(
+  "/api/selections",
+  asyncHandler(async (req, res) => {
+    const { offset = 0, limit = 10, order = "countDesc" } = req.query;
+    const offsetNum = parseInt(offset);
+    const limitNum = parseInt(limit);
 
+    const orderBy = orderByStartup(order);
+    const startups = await prisma.startup.findMany({
+      orderBy,
+      skip: offsetNum,
+      take: limitNum,
+      include: { Category: true },
     });
-    res.status(200).send(select);
-  } catch(error) {res.status(400).send({message:error.message});}
-})
+
+    const responseData = await paginationHandler(startups, offsetNum, limitNum);
+
+    // BigInt 값을 문자열로 변환하여 JSON 응답 생성
+    res.send(JSON.stringify(responseData, replacer));
+  })
+);
 
 //전체 투자 현황 조회
-app.get("/investments", async(req, res) => {
-  const {offset = 0, limit =10} =req.query;
-  try{
-    const invest = await prisma.mockInvestor.findMany({
-      orderBy: {id: "asc"},
-      skip: parseInt(offset),
-      take:parseInt(limit),
-    });
-    const serializedInvest = JSON.stringify(invest, replacer); res.send(serializedInvest);
-  }catch(error) {res.status(401).send({message:error.message});}
-})
+app.get(
+  "/api/investments",
+  asyncHandler(async (req, res) => {
+    const { offset = 0, limit = 10, order = "simInvestDesc" } = req.query;
+    const offsetNum = parseInt(offset);
+    const limitNum = parseInt(limit);
 
+    const orderBy = orderByStartup(order);
+    const startups = await prisma.startup.findMany({
+      orderBy,
+      skip: offsetNum,
+      take: limitNum,
+      include: { Category: true },
+    });
+
+    const responseData = await paginationHandler(startups, offsetNum, limitNum);
+
+    // BigInt 값을 문자열로 변환하여 JSON 응답 생성
+    res.send(JSON.stringify(responseData, replacer));
+  })
+);
 //투자자 상제 조회 
 app.get("/investments/:id", async (req, res) => {
   const {id} = req.params;
@@ -160,48 +191,53 @@ app.get("/investments/:id", async (req, res) => {
 
 
 //특정 기업에 투자
-app.post("/investments", async(req, res) => {
+app.post("/api/investments", async (req, res) => {
   assert(req.body, CreateInvest);
-  try{
+  try {
     const createdInvest = await prisma.mockInvestor.create({
       data: req.body,
     });
-    const serializedInvest = JSON.stringify(createdInvest, replacer); res.send(serializedInvest);
-  }catch(error) {res.status(400).send({message: error.message}); }
-})
+    const serializedInvest = JSON.stringify(createdInvest, replacer);
+    res.send(serializedInvest);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
 
 
 //투자 수정
-app.patch("/investments/:id", async(req, res) => {
-  const {id} = req.params;
+app.patch("/api/investments/:id", async (req, res) => {
+  const { id } = req.params;
   const numId = parseInt(id, 10);
-  assert (req.body, PatchInvest)
-  try{
+  assert(req.body, PatchInvest);
+  try {
     const updateInvest = await prisma.mockInvestor.update({
       data: req.body,
+      where: {
+        startupId: numId,
+      },
+    });
+    const serializedStartups = JSON.stringify(updateInvest, replacer);
+    res.send(serializedStartups);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
+  //투자 삭제
+  app.delete("/api/investments/:id", async (req, res) => {
+    const { id } = req.params;
+    const numId = parseInt(id, 10);
+    const deleteInvest = await prisma.mockInvestor.findUnique({
       where: {
         id: numId,
       },
     });
-    const serializedStartups = JSON.stringify(updateInvest, replacer); res.send(serializedStartups);
-  }catch(error){res.status(404).send({message: error.message}); }
-})
-
-  //투자 삭제
-  app.delete("/investments/:id", async(req,res) => {
-    const {id} = req.params;
-    const numId = parseInt(id, 10);
-      const deleteInvest = await prisma.mockInvestor.findUnique({
-        where: {
-          id: numId
-        },
-      });
-      if(!deleteInvest){
-        return res.status(404).send({message: "투자가 존재하지 않습니다"});
-      }
-      await prisma.mockInvestor.delete({where: {id: numId}});
-      return res.status(200).send({message: "게시글이 삭제 되었습니다"});
-  })
+    if (!deleteInvest) {
+      return res.status(404).send({ message: "투자가 존재하지 않습니다" });
+    }
+    await prisma.mockInvestor.delete({ where: { id: numId } });
+    return res.status(200).send({ message: "게시글이 삭제 되었습니다" });
+  });
 
 // 프론트랑 겹치니깐 8000으로 바꾼다.
 const port = process.env.PORT || 8000;
